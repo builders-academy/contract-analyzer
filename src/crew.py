e91bff8a80f8ab4cef1a71f36c8825c73c3cd5e4
Biwas Bhandari <biwas2059@gmail.com>
2024-08-09 16:01:00 +0545

    fix(pysqlite error)

--- src/crew.py
+++ src/crew.py
@@ -1,3 +1,9 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
+
 import streamlit as st
 from crewai import Crew, Process
 from agents import SmartContractAnalysisAgents
